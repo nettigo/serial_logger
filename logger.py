44b80e983b30d569954c9d18a35f3fe30bacd189
Witold Rugowski <rugowski@nhw.pl>
2013-07-11 23:22:02 +0200

    poprawki

--- logger.py
+++ logger.py
@@ -4,12 +4,15 @@ import serial
 ser = serial.Serial('/dev/ttyACM0',9600, timeout=1)
 if ser.isOpen():
     print("Otwarty")
-sleep(2)
+else:
+    exit(0)
+ser.flushInput()
 while True:
     line = ''
     while ser.inWaiting() > 0:
         ret = ser.read(1)
         line += ret
+        sleep(0.01)
         if ret == '\n':
             break
     if line != '':
