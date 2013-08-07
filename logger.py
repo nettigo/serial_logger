948ceb3fcd7ccc453883f675da696899217fa44e
Witold Rugowski <rugowski@nhw.pl>
2013-08-07 13:18:33 +0200

    use argparse to get list of serial ports

--- logger.py
+++ logger.py
@@ -1,7 +1,42 @@
 from __future__ import print_function
 from time import gmtime, strftime, sleep
 import serial
-ser = serial.Serial('/dev/ttyACM0',9600, timeout=1)
+import os
+from serial.tools import list_ports
+import argparse
+
+import argparse
+
+def list_serial_ports():
+    # Windows
+    if os.name == 'nt':
+        # Scan for available ports.
+        available = []
+        for i in range(256):
+            try:
+                s = serial.Serial(i)
+                available.append('COM'+str(i + 1))
+                s.close()
+            except serial.SerialException:
+                pass
+        return available
+    else:
+        # Mac / Linux
+        return [port[0] for port in list_ports.comports()]
+def print_serial_ports():
+    ret = list_serial_ports()
+    print('\n'.join(ret))
+parser = argparse.ArgumentParser()
+parser.add_argument("-l", "--list", help='List serial ports and stop', action='store_true')
+parser.add_argument("-p", "--port", help='Serial port used to listen to', default='/dev/ttyACM0')
+args = parser.parse_args()
+if args.list:
+    print_serial_ports();
+    exit(0);
+
+port=args.port
+    
+ser = serial.Serial(port,9600, timeout=1)
 if ser.isOpen():
     print("Otwarty")
 else:
